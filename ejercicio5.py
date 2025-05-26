1d75ea76e262897e15bd8e132040ff25daf318c5
Andres18-pixel <ericksonlopez@uamv.edu.ni>
2025-05-26 10:47:55 -0600

    Compuse este ejercicio

--- ejercicio5.py
+++ ejercicio5.py
@@ -4,10 +4,10 @@ ventas = [
     [27, 34, 29, 31]
 ]
 
-zonas = [sum([ventas[v][z] for v in range(3)]) for z in range(4)]
+zonas = [sum([ventas[x][y] for x in range(3)]) for y in range(4)]
 zona_max = zonas.index(max(zonas)) + 1
 
-vendedor_ventas = [sum(ventas[v]) for v in range(3)]
+vendedor_ventas = [sum(ventas[x]) for x in range(3)]
 vendedor_min = vendedor_ventas.index(min(vendedor_ventas)) + 1
 
 total = sum(sum(fila) for fila in ventas)
